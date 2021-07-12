4c7887bf4613891e63aebc0efd91782685dff494
Diksha0411 <vanshikasachdeva13@gmail.com>
2021-07-12 14:03:58 +0530

    covid website

---
+++ css/style.php
@@ -0,0 +1,52 @@
+<style type="text/css">
+html
+{
+	scroll-behavior:smooth ;
+}
+*{margin: 0 ; padding: 0; box-sizing: border-box; font-family: 'Mulish', sans-serif;}
+
+#h{
+	color: red;
+}
+.nav_style{
+	background-color: #a29bfe!important;
+}
+.nav_style a{color : white ;}
+.a img{
+	animation: corona 3s linear infinite;
+}
+@keyframes corona
+{
+	0% {transform: rotate(0);}
+	100% {transform: rotate(360deg);}
+}
+.sub_section{
+	background-color:#F5EEF8 ; 
+}
+.sub_section{
+	background-color:#F5EEF8; 
+}
+
+#tbval
+{
+	background-color:#87E7C7 ;
+}
+#button
+{
+	display:none;
+	position: fixed;
+	bottom: 30px;
+	right:40px;
+	z-index: 99;
+	border: none;
+	background-color:black
+	color: white;
+	cursor: pointer;
+	padding: 10px;
+	border-radius: 10px;
+}
+#button:hover
+{
+	background:#606060;
+}
+</style>
